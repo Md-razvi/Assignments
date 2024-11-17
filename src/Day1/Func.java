b69638fc921308cf0644ab1dcbe78800347c5af7
Md-razvi <mraza1748@gmail.com>
2024-11-17 16:13:28 +0530

    Updated Changes 1

---
+++ src/Day1/Func.java
@@ -0,0 +1,12 @@
+package Day1;
+
+public class Func {
+    public static void main (String[] args) {
+        //Write a function to print your name and call the function from main method
+        PrintFunction("Mudabir Raza");
+    }
+    public static void PrintFunction(String name){
+        System.out.println("My name is "+name);
+    }
+
+}
