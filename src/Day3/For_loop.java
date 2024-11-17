f25e931f7ab5fbcb9593bedf2ca140e6986cec3c
Md-razvi <mraza1748@gmail.com>
2024-11-18 00:55:04 +0530

    Day3

---
+++ src/Day3/For_loop.java
@@ -0,0 +1,11 @@
+package Day3;
+
+public class For_loop {
+    public static void main(String[] args)
+    {
+        for (int a=1;a<=10;a++)
+        {
+            System.out.println(a+". Bright IT Careers");
+        }
+    }
+}
---
+++ src/Day3/Largest_numbers.java
@@ -0,0 +1,15 @@
+package Day3;
+import java.util.Scanner;
+public class Largest_numbers {
+    public static void main(String[] args)
+    {
+        System.out.println("enter 3 numbers");
+        Scanner n=new Scanner(System.in);
+        int a=n.nextInt();
+        int b=n.nextInt();
+        int c=n.nextInt();
+        int max=a>b?a:b;
+        int max2=max>c?max:c;
+        System.out.println("The largest of the 3 is "+max2);
+    }
+}
