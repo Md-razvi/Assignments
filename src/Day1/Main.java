cf9b9c0fd2674b406f42eca1624e80e2546d8569
Md-razvi <mraza1748@gmail.com>
2024-11-17 15:43:54 +0530

    Day2

--- src/Day1/Main.java
+++ src/Day1/Main.java
@@ -1,3 +1,5 @@
+package Day1;
+
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
---
+++ src/Day2/Assign2.java
@@ -0,0 +1,7 @@
+package Day2;
+
+public class Assign2 {
+    public static void main(String[] args){
+        System.out.println();
+    }
+}
