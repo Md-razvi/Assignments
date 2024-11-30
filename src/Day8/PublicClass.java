79d39d53381134da52bfd6f87c65b401db29a8bc
Md-razvi <mraza1748@gmail.com>
2024-12-01 00:05:31 +0530

    Day 7

---
+++ src/Day8/PublicClass.java
@@ -0,0 +1,10 @@
+package Day8;
+
+public class PublicClass {
+    //public Fields
+    public int a=32;
+    //public methods
+    public void  method(){
+        System.out.println("this is a public method");
+    }
+}
---
+++ src/Day8/pack2/ChildSubClass.java
@@ -0,0 +1,10 @@
+package Day8.pack2;
+import Day8.ProtectedFields;
+
+public class ChildSubClass extends ProtectedFields {
+    public static void main(String[] args){
+        ChildSubClass obj1=new ChildSubClass();
+        obj1.name="this is a protexted field";
+
+    }
+}
